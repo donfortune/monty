f4ee4a4bcccf46fb7db4cf2e3256eb1bf7263f63
Under20techie <under20techie@gmail.com>
2023-12-13 21:09:51 +0100

    you

--- get_add.c
+++ get_add.c
@@ -1,37 +1,30 @@
 #include "monty.h"
 
 /**
- * get_div - function that divides the top two elements of the stack
+ * get_add - function that adds the top two elements of the stack
  * @stack: pointer to the top of the stack
  * @line_number: where the line number appears
- * Description: 7. div
+ * Description: 4. add
  * Return: see below
  * 1. upon success, nothing
  * 2. upon fail, EXIT_FAILURE
  */
-void get_div(stack_t **stack, size_t line_number)
+void get_add(stack_t **stack, size_t line_number)
 {
 	stack_t *first, *second;
 
 	if ((*stack == NULL) || ((*stack)->next == NULL))
 	{
-		fprintf(stderr, "L%lu: can't div, stack too short\n", line_number);
+		fprintf(stderr, "L%lu: can't add, stack too short\n", line_number);
 		fclose(file);
 		free(*stack);
-		exit(EXIT_FAILURE);
-	}
-	else if ((*stack)->n == 0)
-	{
-		fprintf(stderr, "L%lu: division by zero\n", line_number);
-		fclose(file);
-		get_free(*stack);
 		exit(EXIT_FAILURE);
 	}
 	else
 	{
 		first = *stack;
 		second = first->next;
-		second->n /= first->n;
+		second->n += first->n;
 		*stack = second;
 		(*stack)->prev = NULL;
 		free(first);
