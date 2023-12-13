9a6989545abe29ff0cc305ac9072116779e72a73
Under20techie <under20techie@gmail.com>
2023-12-13 20:49:24 +0100

    you

---
+++ get_nop.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+
+/**
+ *  get_nop - When no operation present
+ *
+ *  @stack: Header of stack
+ *
+ *  @line_number: Integer of line numbers
+ *
+ *  Return: Void
+ */
+
+
+void get_nop(stack_t **stack, size_t line_number)
+{
+
+	(void) stack;
+	(void) line_number;
+}
